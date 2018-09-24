93879d25ae2e6b31fa4b8286ca53a729b7b23bec
ali <aliahmedcs1@gmail.com>
2018-09-24 12:59:21 +0200

    Initial commit

---
+++ src/main/java/com/magdsoft/modon/ModonApplication.java
@@ -0,0 +1,12 @@
+package com.magdsoft.modon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModonApplication.class, args);
+	}
+}
